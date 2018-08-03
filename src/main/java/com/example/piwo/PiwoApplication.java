15652d448629a0b9674a15ee6343996799c7b822
pylyp.gorpenko@nvt.pl <tzXa9yHV>
2018-08-03 10:27:01 +0200

    create structure

---
+++ src/main/java/com/example/piwo/PiwoApplication.java
@@ -0,0 +1,12 @@
+package com.example.piwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiwoApplication.class, args);
+	}
+}
